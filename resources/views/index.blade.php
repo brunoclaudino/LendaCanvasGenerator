d61fcc505c809d07750ca3c29ae99dcc3659ec24
Washington Pagotto Batista <wstroks@gmail.com>
2019-06-03 03:28:16 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale-1.0"/>
     <title>Canvas Generator</title>
     <link rel="stylesheet" type="text/css" href="{{asset('css/estilo.css')}}"/>
-    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('css/bootstrap/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{ secure_asset('css/AdminLTE.min.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <script type="text/javascript" src="{{asset('js/bootstrap/jquery-3.4.0.min.js.js')}}"></script>
